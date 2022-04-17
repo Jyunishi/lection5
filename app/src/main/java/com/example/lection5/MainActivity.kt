ace779946a0fcfaba17bef954286e359b468b62d
Admin <satim2012@gmail.com>
2022-04-17 12:50:18 +0500

    orientation config change add

--- app/src/main/java/com/example/lection5/MainActivity.kt
+++ app/src/main/java/com/example/lection5/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.lection5
 
+import android.content.IntentFilter
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    //private val appReceiver: MyCustomReceiver = MyCustomReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val intentFilter: IntentFilter = IntentFilter()
+        //this.registerReceiver(appReceiver, intentFilter)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val button: Button = findViewById<Button>(R.id.myButton)
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            button.textSize = 21F
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            button.textSize = 10F
+        }
     }
 }
---
+++ app/src/main/java/com/example/lection5/MyCustomReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.lection5
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyCustomReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("MyCustomReceiver.onReceive() is not implemented")
+    }
+}
